b4d2f0ac97313c78b76553360036794eee1e7a5d
Rabian Ikwan <rabianikwan67@gmail.com>
2023-07-14 18:18:57 +0800

    feat(#2): add serverAuth.ts

---
+++ lib/serverAuth.ts
@@ -0,0 +1,8 @@
+import { NextApiRequest } from "next";
+import { getSession } from "next-auth/react";
+
+import prismadb from "@/lib/prismadb";
+
+const serverAuth = async (req: NextApiRequest) => {
+    const session = await  getSession({ req });
+}
