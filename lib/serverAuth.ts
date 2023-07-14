3288ab12e2c224d9d1a5301c46b99baea3548f1e
Rabian Ikwan <rabianikwan67@gmail.com>
2023-07-14 20:03:23 +0800

    feat(#2): save session from user

--- lib/serverAuth.ts
+++ lib/serverAuth.ts
@@ -4,5 +4,26 @@ import { getSession } from "next-auth/react";
 import prismadb from "@/lib/prismadb";
 
 const serverAuth = async (req: NextApiRequest) => {
-    const session = await  getSession({ req });
-}
+    const session = await getSession({req})
+
+    // if there is no email in session
+    if (!session?.user?.email) {
+        throw new Error('Not signed in')
+    }
+
+    // check current user has registered
+    const currentUser = await prismadb.user.findUnique({
+        where : {
+            email : session.user?.email,
+        }
+    });
+
+    // if not registered throw error not sign in
+    if(!currentUser) {
+        throw new Error('Not signed in')
+    }
+
+    return { currentUser }
+};
+
+export default serverAuth;
