0ae493d6bbccf716d94f42b95fe19d95994f8a62
Rabian Ikwan <rabianikwan67@gmail.com>
2023-07-14 20:03:02 +0800

    feat(#2): protect dashboard page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,38 @@
+import {getSession, signOut} from "next-auth/react";
+import {NextPageContext} from "next";
+
+export async function getServerSideProps(context: NextPageContext) {
+    const session = await getSession(context);
+
+
+    //check if current session is exist if not protect '/' routes
+    if(!session) {
+        return {
+            redirect: {
+                destination : '/auth',
+                permanent: false
+            }
+        }
+    }
+
+    return  {
+        props: {}
+    }
+}
 
 export default function Home() {
+    //protect dashboard route
+
   return (
-    <main>
-      <h1 className="text-white text-6xl">Login Succesfull!!</h1>
-    </main>
+    <div>
+      <h1 className="text-white text-4xl">
+          Login Succesfull!!
+      </h1>
+        <button
+        className="h-10 w-20 bg-white"
+        onClick={() => signOut()}>
+            Sign Out
+        </button>
+    </div>
   )
 }
